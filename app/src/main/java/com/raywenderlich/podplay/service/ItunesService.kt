ac5c3d1d230181bddfbfe20ac3c33aede2ed2780
rachaelfloto <rfloto2@live.spcollege.edu>
2021-10-02 00:28:22 -0500

    page 478

---
+++ app/src/main/java/com/raywenderlich/podplay/service/ItunesService.kt
@@ -0,0 +1,25 @@
+package com.raywenderlich.podplay.service
+
+import com.raywenderlich.podplay.viewmodel.SearchViewModel
+import kotlinx.coroutines.Deferred
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ItunesService {
+    @GET("/search?media=podcast")
+    suspend fun searchPodcastByTerm(@Query("term") term: String): Response<PodcastResponse>
+
+    companion object {
+        val instance: ItunesService by lazy {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://itunes.apple.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            retrofit.create(ItunesService::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -0,0 +1,117 @@
+package com.raywenderlich.podplay.ui
+
+
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.widget.SearchView
+import android.view.Menu
+import android.view.View
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.raywenderlich.podplay.R
+import com.raywenderlich.podplay.adapter.PodcastListAdapter
+import com.raywenderlich.podplay.adapter.PodcastListAdapter.PodcastListAdapterListener
+import com.raywenderlich.podplay.databinding.ActivityPodcastBinding
+import com.raywenderlich.podplay.repository.ItunesRepo
+import com.raywenderlich.podplay.service.ItunesService
+import com.raywenderlich.podplay.viewmodel.SearchViewModel
+import com.raywenderlich.podplay.viewmodel.SearchViewModel.PodcastSummaryViewData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class PodcastActivity : AppCompatActivity(), PodcastListAdapterListener {
+
+    private val searchViewModel by viewModels<SearchViewModel>()
+    private lateinit var podcastListAdapter: PodcastListAdapter
+    private lateinit var databinding: ActivityPodcastBinding
+
+    private val TAG = javaClass.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        databinding = ActivityPodcastBinding.inflate(layoutInflater)
+        setContentView(databinding.root)
+        setupToolbar()
+        setupViewModels()
+        updateControls()
+        handleIntent(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        val searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        return true
+    }
+
+    private fun performSearch(term: String) {
+        showProgressBar()
+        GlobalScope.launch {
+            val results = searchViewModel.searchPodcasts(term)
+            withContext(Dispatchers.Main) {
+                hideProgressBar()
+                databinding.toolbar.title = term
+                podcastListAdapter.setSearchData(results)
+            }
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?: return
+            performSearch(query)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(databinding.toolbar)
+    }
+
+    private fun setupViewModels() {
+        val service = ItunesService.instance
+        searchViewModel.iTunesRepo = ItunesRepo(service)
+    }
+
+    private fun updateControls() {
+        databinding.podcastRecyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(this)
+        databinding.podcastRecyclerView.layoutManager = layoutManager
+
+        val dividerItemDecoration = DividerItemDecoration(
+            databinding.podcastRecyclerView.context, layoutManager.orientation)
+        databinding.podcastRecyclerView.addItemDecoration(dividerItemDecoration)
+
+        podcastListAdapter = PodcastListAdapter(null, this, this)
+        databinding.podcastRecyclerView.adapter = podcastListAdapter
+    }
+
+    override fun onShowDetails(podcastSummaryViewData: PodcastSummaryViewData) {
+        // Not implemented yet
+    }
+
+    private fun showProgressBar() {
+        databinding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        databinding.progressBar.visibility = View.INVISIBLE
+    }
+}
