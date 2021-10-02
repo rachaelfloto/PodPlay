0aa285d5d07e2181b372d353c1f190926f6a14bb
rachaelfloto <rfloto2@live.spcollege.edu>
2021-10-02 01:27:53 -0500

    chap 21 end

--- app/src/main/java/com/raywenderlich/podplay/util/DateUtils.kt
+++ app/src/main/java/com/raywenderlich/podplay/util/DateUtils.kt
@@ -4,7 +4,6 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-class DateUtils {
     object DateUtils {
         fun jsonDateToShortDate(jsonDate: String?): String {
             //1
@@ -22,4 +21,4 @@ class DateUtils {
                     Locale.getDefault())
             // 6
             return outputFormat.format(date)
-        }}}
+        }}
--- app/src/main/java/com/raywenderlich/podplay/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/raywenderlich/podplay/viewmodel/SearchViewModel.kt
@@ -1,13 +1,11 @@
 package com.raywenderlich.podplay.viewmodel
 
-import android.app.Application
 
+import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.raywenderlich.podplay.repository.ItunesRepo
 import com.raywenderlich.podplay.service.PodcastResponse
 import com.raywenderlich.podplay.util.DateUtils
-
-import com.raywenderlich.podplay.util.DateUtils.DateUtils.jsonDateToShortDate
 
 class SearchViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -24,27 +22,22 @@ class SearchViewModel(application: Application) : AndroidViewModel(application) 
             PodcastSummaryViewData {
         return PodcastSummaryViewData(
             itunesPodcast.collectionCensoredName,
-            jsonDateToShortDate(itunesPodcast.releaseDate),
+            DateUtils.jsonDateToShortDate(itunesPodcast.releaseDate),
             itunesPodcast.artworkUrl30,
             itunesPodcast.feedUrl)
     }
-    // 1
-    suspend fun searchPodcasts(term: String):
-            List<PodcastSummaryViewData> {
-        // 2
+
+    suspend fun searchPodcasts(term: String): List<PodcastSummaryViewData> {
         val results = iTunesRepo?.searchByTerm(term)
-        // 3
+
         if (results != null && results.isSuccessful) {
-            // 4
             val podcasts = results.body()?.results
-            // 5
             if (!podcasts.isNullOrEmpty()) {
-                // 6
                 return podcasts.map { podcast ->
                     itunesPodcastToPodcastSummaryView(podcast)
                 }
             }
         }
-        // 7
         return emptyList()
-    }}
+    }
+}
